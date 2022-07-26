e699090324c36622b57e7755d8c3fa02212259a7
Reinaldo <naldos15@hotmail.com>
2022-07-26 10:14:10 -0300

    first commit

---
+++ src/main/java/com/reinaldo/os/services/DBService.java
@@ -0,0 +1,37 @@
+package com.reinaldo.os.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.reinaldo.os.domain.Cliente;
+import com.reinaldo.os.domain.OS;
+import com.reinaldo.os.domain.Tecnico;
+import com.reinaldo.os.domain.enums.Prioridade;
+import com.reinaldo.os.domain.enums.Status;
+import com.reinaldo.os.repositories.ClienteRepository;
+import com.reinaldo.os.repositories.OSRepository;
+import com.reinaldo.os.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private OSRepository osRepository;
+	
+	public void instanciaDB() {
+		Tecnico t1 = new Tecnico(null, "Valdir Cezar", "973.791.690-50", "(62) 99306-1196");
+		Cliente c1 = new Cliente(null, "Betina Campos", "885.702.810-05", "(62) 99404-7729");
+		
+		OS os1 = new OS(null, Prioridade.ALTA, "Teste create OS", Status.ANDAMENTO, t1, c1);
+
+		tecnicoRepository.saveAll(Arrays.asList(t1));
+		clienteRepository.saveAll(Arrays.asList(c1));
+		osRepository.saveAll(Arrays.asList(os1));
+	}
+}
