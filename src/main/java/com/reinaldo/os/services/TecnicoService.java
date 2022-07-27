0847701c7acb75fda9e4689dc3d09c2a397bf14d
Reinaldo <naldos15@hotmail.com>
2022-07-27 10:39:19 -0300

    delete e exceptions funcionam

--- src/main/java/com/reinaldo/os/services/TecnicoService.java
+++ src/main/java/com/reinaldo/os/services/TecnicoService.java
@@ -19,49 +19,55 @@ public class TecnicoService {
 
 	@Autowired
 	private TecnicoRepository repository;
-	
+
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! " + id + ", Tipo: " + Tecnico.class.getName()) );
+				"Objeto não encontrado! " + id + ", Tipo: " + Tecnico.class.getName()));
 	}
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
 	}
-	
+
 	public Tecnico create(TecnicoDTO objDTO) {
-		if(findByCPF(objDTO) != null) {
+		if (findByCPF(objDTO) != null) {
 			throw new DataIntegrityViolationException("CPF já cadastrado na base de dados");
 		}
-		
+
 		Tecnico newObj = new Tecnico(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone());
 		return repository.save(newObj);
 	}
-	
+
 	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
-		 Tecnico oldObj = findById(id);
-		 
-		 if(findByCPF(objDTO) != null && findByCPF(objDTO).getId()  != id) {
-			 throw new DataIntegrityViolationException("CPF já cadastrado na base de dados");
-		 }
-		 
-		 oldObj.setNome(objDTO.getNome());
-		 oldObj.setCpf(objDTO.getCpf());
-		 oldObj.setTelefone(objDTO.getTelefone());
-		 return repository.save(oldObj);
+		Tecnico oldObj = findById(id);
+
+		if (findByCPF(objDTO) != null && findByCPF(objDTO).getId() != id) {
+			throw new DataIntegrityViolationException("CPF já cadastrado na base de dados");
+		}
+
+		oldObj.setNome(objDTO.getNome());
+		oldObj.setCpf(objDTO.getCpf());
+		oldObj.setTelefone(objDTO.getTelefone());
+		return repository.save(oldObj);
 	}
-	
+
+	public void delete(Integer id) {
+		Tecnico obj =  findById(id);
+		
+		if(obj.getList().size() > 0) {
+			throw new DataIntegrityViolationException("Técnico possui Ordens de Serviço. Não pode ser deletado!");
+		}
+		
+		repository.deleteById(id);
+	}
+
 	private Tecnico findByCPF(TecnicoDTO objDTO) {
 		Tecnico obj = repository.findByCPF(objDTO.getCpf());
-		if(obj != null) {
+		if (obj != null) {
 			return obj;
 		}
 		return null;
 	}
 
-	
-	
-	
-	
 }
