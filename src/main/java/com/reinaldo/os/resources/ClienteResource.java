a9673dcd8ba6937fe9b9e84eb0bbab6c98bcf62b
Reinaldo <naldos15@hotmail.com>
2022-08-03 23:12:08 -0300

    post method to cliente

--- src/main/java/com/reinaldo/os/resources/ClienteResource.java
+++ src/main/java/com/reinaldo/os/resources/ClienteResource.java
@@ -1,13 +1,21 @@
 package com.reinaldo.os.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.reinaldo.os.domain.Cliente;
+import com.reinaldo.os.domain.Tecnico;
 import com.reinaldo.os.dtos.ClienteDTO;
 import com.reinaldo.os.services.ClienteService;
 
@@ -20,9 +28,24 @@ public class ClienteResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<ClienteDTO> findByID(@PathVariable Integer id){
-		Cliente obj = service.findByID(id);
+		Cliente obj = service.findById(id);
 		ClienteDTO objDTO = new ClienteDTO(obj);
 		return ResponseEntity.ok().body(objDTO);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<ClienteDTO>> findAll(){
+		List<ClienteDTO> obj = service.findAll().stream().
+				map(objDTO -> new ClienteDTO(objDTO)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClienteDTO> created(@RequestBody ClienteDTO objDTO){
+		Cliente newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
--- src/main/java/com/reinaldo/os/services/ClienteService.java
+++ src/main/java/com/reinaldo/os/services/ClienteService.java
@@ -1,12 +1,18 @@
 package com.reinaldo.os.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.reinaldo.os.domain.Cliente;
+import com.reinaldo.os.domain.Pessoa;
+import com.reinaldo.os.domain.Tecnico;
+import com.reinaldo.os.dtos.ClienteDTO;
+import com.reinaldo.os.dtos.TecnicoDTO;
 import com.reinaldo.os.repositories.ClienteRepository;
+import com.reinaldo.os.services.exceptions.DataIntegrityViolationException;
 import com.reinaldo.os.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -15,11 +21,30 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repository;
 
-	public Cliente findByID(Integer id) {
+	public Cliente findById(Integer id) {
 		Optional<Cliente> obj = repository.findById(id);
-		//return obj.orElseThrow( () -> new ObjectNotFoundException(
-			//"Objeto não encontrado! " + id + ", Tipo: " + Cliente.class.getName()));		
-		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! " + id + ", Tipo: " + Tecnico.class.getName()));
+	}
+	
+	public List<Cliente> findAll(){
+		return repository.findAll();
+	}
+	
+	public Cliente create(ClienteDTO objDTO) {
+		if (findByCPF(objDTO) != null) {
+			throw new DataIntegrityViolationException("CPF já cadastrado na base de dados");
+		}
+		Cliente newObj = new Cliente(null, objDTO.getNome(), objDTO.getCpf(), objDTO.getTelefone());
+		return repository.save(newObj);
+	}
+	
+	private Pessoa findByCPF(ClienteDTO objDTO) {
+		Pessoa obj = repository.findByCPF(objDTO.getCpf());
+		if (obj != null) {
+			return obj;
+		}
+		return null;
 	}
 	
 }
