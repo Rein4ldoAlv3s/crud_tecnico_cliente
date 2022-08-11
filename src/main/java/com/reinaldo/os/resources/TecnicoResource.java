c2762a0a8b69b766d653e8c2a3a3472efa385b59
Reinaldo <naldos15@hotmail.com>
2022-08-11 10:11:10 -0300

    final version api

--- src/main/java/com/reinaldo/os/resources/TecnicoResource.java
+++ src/main/java/com/reinaldo/os/resources/TecnicoResource.java
@@ -10,6 +10,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,6 +27,7 @@ import com.reinaldo.os.dtos.TecnicoDTO;
 import com.reinaldo.os.repositories.TecnicoRepository;
 import com.reinaldo.os.services.TecnicoService;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
